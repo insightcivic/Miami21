ea0457e02238e730cb1f23201da233cf7ff5c430
InisightCivic <128551464+insightcivic@users.noreply.github.com>
2024-10-28 20:28:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ def generate_response(query, retrieved_chunks):
     
     # Create a structured message for the chat completion
     messages = [
-        {"role": "system", "content": "You are an assistant providing information based on the Miami 21 code.
+        {"role": "system", "content": "You are an assistant providing information based on the Miami 21 code."
                                       "Respond strictly based on the information provided in the context. "
                                       "If the context lacks information on a specific question, respond with, "
                                        "'I'm unable to answer based on the information provided.' "
